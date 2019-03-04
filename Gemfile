afb1e10122c2c2e9d60a9314344f501aeba7468b
liuchunyueh <liuchunyueh@gmail.com>
2019-03-04 23:06:12 +0800

    add pagination using Kaminari gem

--- Gemfile
+++ Gemfile
@@ -11,6 +11,8 @@ gem 'carrierwave'
 
 gem 'ffaker'
 
+gem 'kaminari'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.6'
 # Use sqlite3 as the database for Active Record
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,7 +4,7 @@ class Admin::ProductsController < ApplicationController
   before_action :set_product, only: [:edit, :update, :destroy]
 
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(10)
   end
 
   def new
