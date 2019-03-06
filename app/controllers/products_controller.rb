7114c7c692975203f562542e0d9e4c83989d4db3
liuchunyueh <liuchunyueh@gmail.com>
2019-03-06 23:30:45 +0800

    set viewrs page by editing controller and index.html.erb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,13 @@
 class ProductsController < ApplicationController
 
   def index
-    
+    @products = Product.page(params[:page]).per(10)
   end
   
+
+  private
+
+  def product_params
+    params.require(:product).permit(:neme, :image, :description, :price)
+  end
 end
